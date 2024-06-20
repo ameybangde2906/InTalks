6ab579a50076e6c9c7c367d01a2c00542b454edb
AmeyBangde29 <bangdeamey@gmail.com>
2024-06-21 04:03:42 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json({ limit: "900mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions ={
-    origin:"http://localhost:3000",
+    origin:"https://podstream-2cce.onrender.com",
     credentials:true
 }
 app.use(cors(corsOptions))
